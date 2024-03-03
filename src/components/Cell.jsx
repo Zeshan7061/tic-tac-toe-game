b4bc5076e860c836ba98b5770636c87168b3b0b7
Zeeshan Aslam <zeeshan.aslam@alignpx.com>
2024-03-03 16:33:55 +0500

    adds draw and winning functionalites

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,9 +1,9 @@
-const Cell = ({ sign, handleCellClick }) => {
+const Cell = ({ sign = '', handleCellClick = function () {}, winner = false }) => {
 	return (
 		<div
-			className={
-				'w-[90px] h-[90px] rounded-md shadow-lg font-bold text-4xl grid place-content-center cursor-pointer transition-all hover:bg-zinc-700 hover:text-white active:scale-90 bg-white text-black'
-			}
+			className={`w-[90px] h-[90px] rounded-md shadow-lg font-bold text-4xl grid place-content-center cursor-pointer transition-all hover:bg-zinc-700 hover:text-white active:scale-90 ${
+				winner && sign ? 'bg-green-900 text-white' : 'bg-white text-black'
+			}`}
 			onClick={handleCellClick}>
 			{sign}
 		</div>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,6 +5,10 @@ function GameBoard() {
 	const [gridSize] = useState(9);
 	const [cells, setCells] = useState([]);
 	const [playerType, setPlayerType] = useState('');
+	const [winner, setWinner] = useState('');
+	const [draw, setDraw] = useState(false);
+
+	const winningCombos = '012,345,678,036,147,258,048,246';
 
 	const drawGrid = useCallback(() => {
 		const cellCordinates = [];
@@ -19,24 +23,70 @@ function GameBoard() {
 		drawGrid();
 	}, [drawGrid]);
 
-	const playGame = () => {
-		drawGrid();
-		setPlayerType('P1');
-	};
-
 	const handleCellClick = cellPos => {
 		if (!playerType || cells[cellPos]) return;
 
 		const sign = playerType === 'P1' ? 'X' : 'O';
 		const updatedCells = cells.map((cell, i) => (cellPos === i ? sign : cell));
 		setCells(updatedCells);
+
+		if (checkWinner(sign, updatedCells)) return;
+
 		setPlayerType(playerType === 'P1' ? 'P2' : 'P1');
 	};
 
+	const playGame = () => {
+		drawGrid();
+		setPlayerType('P1');
+		setWinner('');
+		setDraw(false);
+	};
+
+	const checkWinner = (cellValue, updatedCells) => {
+		const combos = winningCombos.split(',');
+		let isWinner = false;
+
+		for (const combo of combos) {
+			const comboArr = combo.split('');
+			isWinner = comboArr.every(el => updatedCells[el] === cellValue);
+
+			if (isWinner) {
+				const winnerCells = updatedCells?.map((cell, i) =>
+					comboArr.includes(String(i)) ? cell : ''
+				);
+				setCells(winnerCells);
+
+				setWinner(playerType === 'P1' ? 'Player X' : 'Player O');
+				setPlayerType('');
+				break;
+			}
+		}
+
+		if (!isWinner && updatedCells?.every(val => val)) {
+			setDraw(true);
+			setPlayerType('');
+			isWinner = true;
+		}
+
+		return isWinner;
+	};
+
 	return (
-		<div className='h-screen w-screen grid place-content-center p-5 bg-slate-300'>
+		<div className='h-screen w-screen grid place-content-center p-5 bg-slate-400'>
 			<h1 className='text-4xl font-bold text-center text-white'>Tic Tac Toe</h1>
 			<hr />
+
+			{draw && (
+				<h1 className='font-extrabold text-center text-3xl text-green-900 animate-bounce my-5'>
+					Game draw!
+				</h1>
+			)}
+
+			{winner && (
+				<h1 className='font-extrabold text-center text-3xl text-green-900 animate-bounce my-5'>
+					{winner} Won!
+				</h1>
+			)}
 
 			{playerType && (
 				<div className='flex gap-3 my-5'>
@@ -59,14 +109,19 @@ function GameBoard() {
 
 			<div className='mt-8 grid grid-cols-3 gap-3'>
 				{cells.map((cell, i) => (
-					<Cell key={i} sign={cell} handleCellClick={() => handleCellClick(i)} />
+					<Cell
+						key={i}
+						sign={cell}
+						winner={winner}
+						handleCellClick={() => handleCellClick(i)}
+					/>
 				))}
 			</div>
 
 			<button
 				className='mt-8 text-2xl bg-white p-3 rounded-md shadow-md font-semibold transition-all hover:bg-zinc-700 hover:text-white active:scale-75'
 				onClick={playGame}>
-				Play
+				Play {(winner || draw) && ' again'}
 			</button>
 		</div>
 	);
